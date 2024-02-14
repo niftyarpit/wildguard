f999f40b5b01caa2e9eac3605b6d0c339a428aac
Arpit <nifty.arpit@gmail.com>
2024-02-14 18:39:42 +0530

    Adding tap & drag gestures to the globe

--- WildGuard/ZoomedView.swift
+++ WildGuard/ZoomedView.swift
@@ -21,8 +21,27 @@ struct ZoomedView: View {
             //let material = UnlitMaterial(color: .red, applyPostProcessToneMap: false)
             let model = ModelEntity(mesh: shape, materials: [material])
             model.components.set(GroundingShadowComponent(castsShadow: true))
+            model.components.set(InputTargetComponent())
+            model.generateCollisionShapes(recursive: false, static: true)
+            //model.collision = CollisionComponent(shapes: [.generateSphere(radius: 0.25)])
             content.add(model)
         }
+        .gesture(TapGesture()
+            .targetedToAnyEntity()
+            .onEnded { val in
+                guard let modelEntity = val.entity as? ModelEntity else { return }
+                let newMaterial = UnlitMaterial(color: .blue)
+                modelEntity.model?.materials = [newMaterial]
+            }
+        )
+        .gesture(DragGesture(minimumDistance: 0)
+            .targetedToAnyEntity()
+            .onChanged { val in
+                guard let parent = val.entity.parent else { return }
+                let newPosition = val.convert(val.location3D, from: .local, to: parent)
+                val.entity.position = [newPosition.x, newPosition.y, val.entity.position.z]
+            }
+        )
     }
 }
 
