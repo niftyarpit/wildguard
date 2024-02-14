04972ff4722b94ca245fb47b3a94c66936439b41
Arpit <nifty.arpit@gmail.com>
2024-02-14 14:29:28 +0530

    creating Globe

--- WildGuard/ZoomedView.swift
+++ WildGuard/ZoomedView.swift
@@ -14,8 +14,11 @@ struct ZoomedView: View {
 
     var body: some View {
         RealityView { content in
-            let shape = MeshResource.generateBox(size: 0.25)
-            let material = SimpleMaterial(color: .red, isMetallic: true)
+            let shape = MeshResource.generateSphere(radius: 0.25)
+            guard let texture = try? await TextureResource(named: "Earth") else { return }
+            var material = UnlitMaterial()
+            material.color = .init(texture: .init(texture))
+            //let material = UnlitMaterial(color: .red, applyPostProcessToneMap: false)
             let model = ModelEntity(mesh: shape, materials: [material])
             model.components.set(GroundingShadowComponent(castsShadow: true))
             content.add(model)
