5bfe4b489615a93fb6a72560857d5cc292d4c518
Arpit <nifty.arpit@gmail.com>
2024-02-15 17:08:19 +0530

    fix full RealityKit movement. try translation on tap gesture

--- WildGuard/ZoomedView.swift
+++ WildGuard/ZoomedView.swift
@@ -16,7 +16,7 @@ struct ZoomedView: View {
     @State private var spinY = 0.0
 
     var body: some View {
-        RealityView { content in
+        RealityView { content, attachments in
             let shape = MeshResource.generateSphere(radius: 0.25)
             guard let texture = try? await TextureResource(named: "Earth") else { return }
             var material = UnlitMaterial()
@@ -26,9 +26,30 @@ struct ZoomedView: View {
             model.components.set(InputTargetComponent())
             model.generateCollisionShapes(recursive: false, static: true)
             content.add(model)
+
+            let pitch = Transform(pitch: Float(animal.mapRotationX * -1)).matrix
+            let yaw = Transform(yaw: Float(animal.mapRotationY)).matrix
+            model.transform.matrix = pitch * yaw
+
+            if let entity = attachments.entity(for: "name") {
+                entity.position = [0, -0.3, 0]
+                content.add(entity)
+            }
+        } update: { content, attachments in
+
+            guard let entity = content.entities.first else { return }
+            let pitch = Transform(pitch: Float((animal.mapRotationX + spinX) * -1)).matrix
+            let yaw = Transform(yaw: Float(animal.mapRotationY + spinY)).matrix
+            entity.transform.matrix = pitch * yaw
+
+        } attachments: {
+            Attachment(id: "name") {
+                Text("Location of \(animal.englishName)")
+                    .font(.extraLargeTitle)
+            }
         }
-        .rotation3DEffect(.radians(spinY), axis: .y)
-        .gesture(DragGesture(minimumDistance: 0)
+        .gesture(
+            DragGesture(minimumDistance: 0)
             .targetedToAnyEntity()
             .onChanged { val in
                 let startLocation = val.convert(val.startLocation3D, from: .local, to: .scene)
@@ -38,10 +59,15 @@ struct ZoomedView: View {
                 spinY = Double(delta.x) * 5
             }
         )
-        .onAppear {
-            spinX = animal.mapRotationX
-            spinY = animal.mapRotationY
-        }
+//        .gesture(
+//            TapGesture()
+//            .targetedToAnyEntity()
+//            .onEnded { value in
+//                var transform = value.entity.transform
+//                transform.translation += [0.1, 0, 0]
+//                value.entity.move(to: transform, relativeTo: value.entity, duration: 3, timingFunction: .easeInOut)
+//            }
+//        )
     }
 }
 
